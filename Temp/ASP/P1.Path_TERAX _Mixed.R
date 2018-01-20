e8a8b482fdf31a84736111b5f8bf7d17bec40de9
anasanz <anasanz.asp@gmail.com>
2018-01-20 19:21:38 +0100

    asp

--- Temp/ASP/P1.Path_TERAX _Mixed.R
+++ Temp/ASP/P1.Path_TERAX _Mixed.R
@@ -130,7 +130,7 @@ PlotPath(e.coefs2
          ,Species.name="PRESENCE \n TERAX"
          ,cex.category = 0.5
          ,plot.axis=FALSE
-         ,text.box.width=c(2, 1))
+         ,estimate.box.width=c(2, 1))
 
 dev.off()
 ####################################### PICAR ##############################
