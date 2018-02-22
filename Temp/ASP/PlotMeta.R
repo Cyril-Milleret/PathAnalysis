b97ea1e8d822e1f388202874fb820a1cf20ecb7e
anasanz <anasanz.asp@gmail.com>
2018-02-22 10:04:12 +0100

    Add field area, plot metamodel and network appendix

---
+++ Temp/ASP/PlotMeta.R
@@ -0,0 +1,156 @@
+
+# Plot of meta-model in Appendix
+rm(list=ls())
+setwd("~/Path analysis/Path_species")
+pdf(file = "Path_Simple.pdf")
+
+  require(sp) 
+  require(plotrix)
+  
+  grid.size.max <- 40
+  grid.size.min <- 0
+  coords <- matrix(c(grid.size.min         , grid.size.min ,
+                     grid.size.max , grid.size.min ,
+                     grid.size.max , grid.size.max ,
+                     grid.size.min          , grid.size.max,
+                     grid.size.min          , grid.size.min
+  ), ncol = 2, byrow = TRUE)
+  
+  P1 <- Polygon(coords)
+  myStudyArea.poly <-  SpatialPolygons(list(Polygons(list(P1), ID = "a")),
+                                       proj4string=CRS("+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+  
+  
+  
+  plot(myStudyArea.poly, xlim=c(-20,70), ylim=c(-30,60), border="white")
+  
+    axis(1)
+    axis(2)
+  
+    # ==== I. DEFINE THE BASIC PATH STRUCTURE ==== # Define position of boxes
+    # ---- 1. Treatment/ PRESENCE ---- 
+    Treatment=cbind(c(-23,-3,-3,-23), c(16,16,23,23)) # 1st column X : bottomleft, bottom right, topright, topleft.# 2nd column Y
+    polygon(Treatment[,1], Treatment[,2], col =  adjustcolor("orange",alpha.f = 0.5), border = "white")
+    text(mean(Treatment[,1]),mean(Treatment[,2]), "TREATMENT", cex = 1)
+    
+    Pres=cbind(c(48,72,72,48), c(16,16,23,23))
+    polygon(Pres[,1], Pres[,2], col =  adjustcolor("blue",alpha.f = 0.5), border = "white")
+    text(mean(Pres[,1]),mean(Pres[,2]), "PRESENCE", cex = 1) 
+    
+    # ---- 2. FOOD ---- 
+    Food=cbind(c(12,32,32,12), c(-10,-10,-3,-3))
+    polygon(Food[,1], Food[,2], col =  adjustcolor("purple",alpha.f = 0.5), border = "white")
+    text(mean(Food[,1]),mean(Food[,2]), "FOOD", cex = 1)
+
+    # ---- 3. VEGETATION  ---- 
+    Height=cbind(c(12,32,32,12), c(40,40,47,47))  
+    polygon(Height[,1], Height[,2], col =  adjustcolor("green",alpha.f = 0.5), border = "white")
+    text(mean(Height[,1]),mean(Height[,2]), "VEGETATION", cex = 1)
+    
+    # ---- 4. LANDSCAPE  ---- 
+    tbl=cbind(c(48,72,72,48), c(-10,-10,-3,-3))
+    polygon(tbl[,1], tbl[,2], col =  adjustcolor("brown",alpha.f = 0.5), border = "white")
+    text(mean(tbl[,1]),mean(tbl[,2]), "LANDSCAPE", cex = 1)
+    
+    
+    # ==== II. ADD THE ARROW ====  
+    
+    # ---- 1. Treatment -> Veg/Food ---- 
+
+    x0=mean(Treatment[1:2,1]) #Veg
+    x1=mean(Height[1,1])
+    y0=mean(Treatment[3,2])
+    y1= mean(Height[c(2:3),2])
+    
+    arrows(x0=x0, x1=x1, y0=y0, y1=y1, length = 0.1, lwd=1, col="grey")
+    draw.circle(mean(c(x0, x1)), mean(c(y0, y1)), radius = 3, border = "darkorange", col = "white")
+    text(mean(c(x0, x1)) , mean(c(y0, y1)),"1", cex=1, col = "darkorange")
+    
+    
+
+    x0=mean(Treatment[1:2,1]) #Food
+    x1=mean(Food[1,1])
+    y0=mean(Treatment[2,2])
+    y1=mean(Food[c(2:3),2])
+    
+    arrows(x0=x0, x1=x1, y0=y0, y1=y1, length = 0.1, lwd=1, col="grey")
+    draw.circle(mean(c(x0, x1)), mean(c(y0, y1)), radius = 3, border = "darkorange", col = "white")
+    text(mean(c(x0, x1)) , mean(c(y0, y1)),"1", cex=1, col = "darkorange")
+    
+    
+
+    # ---- 2. Co-lateral -> Food ---- 
+    
+    x0=mean(tbl[c(1),1]) #Lands
+    x1=mean(Food[c(2),1])
+    y0=mean(Food[2:3,2])-1
+    y1=mean(tbl[2:3,2])-1
+    
+    arrows(x0=x0, x1=x1, y0=y0, y1=y1, length = 0.1, lwd=1, col="grey")
+    draw.circle(mean(c(x0, x1)), mean(c(y0, y1)), radius = 3, border = adjustcolor("purple",alpha.f = 0.5), 
+                col = "white")
+    text(mean(c(x0, x1)) , mean(c(y0, y1)),"2", cex = 1, col = adjustcolor("purple",alpha.f = 0.5))
+   
+    
+    
+    x0=mean(Height[c(1:2),1]) #Veg
+    x1=mean(Height[c(1:2),1])
+    y0=mean(Height[2,2])
+    y1=mean(Food[3,2])
+    
+    arrows(x0=x0, x1=x1, y0=y0, y1=y1, length = 0.1, lwd=1, col="grey")
+    draw.circle(mean(c(x0, x1)), mean(c(y0, y1)), radius = 3, border = adjustcolor("purple",alpha.f = 0.5),
+                col = "white")
+    text(mean(c(x0, x1)) , mean(c(y0, y1)),"2", cex = 1, col = adjustcolor("purple",alpha.f = 0.5))
+  
+    
+    # ---- 3. Veg/Food/lands -> Presence ---- 
+    
+    x0=mean(Height[c(2),1]) #Veg
+    x1=mean(Pres[c(1:2),1])
+    y0=mean(Height[2:3,2])
+    y1=mean(Pres[3,2])
+    
+    arrows(x0=x0, x1=x1, y0=y0, y1=y1, length = 0.1, lwd=1, col="grey")
+    draw.circle(mean(c(x0, x1)), mean(c(y0, y1)), radius = 3, border = adjustcolor("blue",alpha.f = 0.5),
+                col = "white")
+    text(mean(c(x0, x1)) , mean(c(y0, y1)),"3", cex = 1, col = adjustcolor("blue",alpha.f = 0.5))
+
+    
+   
+    
+    x0=mean(Food[c(2),1]) #Food
+    x1=mean(Pres[c(1:2),1])-1
+    y0=mean(Food[2:3,2])
+    y1=mean(Pres[2,2])
+    
+    arrows(x0=x0, x1=x1, y0=y0, y1=y1, length = 0.1, lwd=1, col="grey")
+    draw.circle(mean(c(x0, x1)), mean(c(y0, y1)), radius = 3, border = adjustcolor("blue",alpha.f = 0.5),
+                col = "white")
+    text(mean(c(x0, x1)) , mean(c(y0, y1)),"3", cex = 1, col = adjustcolor("blue",alpha.f = 0.5))
+
+    
+
+    
+    x0=mean(Pres[1:2,1]) #Lands
+    x1=mean(Pres[1:2,1])
+    y0=mean(tbl[3,2])
+    y1=mean(Pres[c(2),2])
+    
+    arrows(x0=x0, x1=x1, y0=y0, y1=y1, length = 0.1, lwd=1, col="grey")
+    draw.circle(mean(c(x0, x1)), mean(c(y0, y1)), radius = 3, border = adjustcolor("blue",alpha.f = 0.5),
+                col = "white")
+    text(mean(c(x0, x1)) , mean(c(y0, y1)),"3", cex = 1, col = adjustcolor("blue",alpha.f = 0.5))
+
+    
+
+
+    
+
+    
+
+    dev.off()
+
+
+
+    
---
+++ Temp/ASP/PlotNetwork.r
@@ -0,0 +1,355 @@
+
+
+  offset.poly = 2 
+  cex.text = 0.6              
+  cex.text1 = 0.75
+  cex.category = 0.6 
+  Treatment.name="TREATMENT"  
+  Species.name="SPECIES"
+  
+  setwd("~/Path analysis/Path_species")
+  pdf(file = "Path_Network.pdf")
+  
+  require(sp)
+  require(plotrix)
+  
+  grid.size.max <- 40
+  grid.size.min <- 0
+  coords <- matrix(c(grid.size.min         , grid.size.min ,
+                     grid.size.max , grid.size.min ,
+                     grid.size.max , grid.size.max ,
+                     grid.size.min          , grid.size.max,
+                     grid.size.min          , grid.size.min
+  ), ncol = 2, byrow = TRUE)
+  
+  P1 <- Polygon(coords)
+  myStudyArea.poly <-  SpatialPolygons(list(Polygons(list(P1), ID = "a")),
+                                       proj4string=CRS("+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+  
+  
+  
+  plot(myStudyArea.poly, xlim=c(-20,70), ylim=c(-30,60), border="white")
+  
+
+    #axis(1)
+    #axis(2)
+ 
+
+  
+  # ==== I. DEFINE THE BASIC PATH STRUCTURE ==== # Define position of boxes
+  # ---- 1. Treatment/ PRESENCE ---- 
+  Treatment=cbind(c(-20,-6,-6,-20), c(16,16,23,23)) # 1st column X : bottomleft, bottom right, topright, topleft.# 2nd column Y
+  polygon(Treatment[,1], Treatment[,2], col =  adjustcolor("orange",alpha.f = 0.5), border = "white")
+  text(mean(Treatment[,1]),mean(Treatment[,2]), Treatment.name, cex = cex.text1)
+  
+  Pres=cbind(c(51,69,69,51), c(16,16,23,23))
+  polygon(Pres[,1], Pres[,2], col =  adjustcolor("blue",alpha.f = 0.5), border = "white")
+  text(mean(Pres[,1]),mean(Pres[,2]), Species.name, cex = cex.text1) 
+  
+  
+  # ---- 2. FOOD ---- 
+  SAI_sd=cbind(c(15,29,29,15), c(10,10,14,14))
+  LAI_sd=cbind(c(15,29,29,15), c(4,4,8,8))
+  biom=cbind(c(15,29,29,15), c(-4,-4,0,0))
+  
+  food= rbind(SAI_sd,LAI_sd,biom)
+  min1 <- min(food[,1])-offset.poly
+  min2 <- min(food[,2])-offset.poly
+  max1 <- max(food[,1])+offset.poly
+  max2 <- max(food[,2])+4
+  
+  
+  polygon(c(min1,max1,max1,min1) ,
+          c(min2,min2,max2,max2)  ,
+          col =  adjustcolor("purple",alpha.f = 0.1), border = "white")
+  text(mean(food[,1]),max(food[,2])+2 , "FOOD", col=grey(0.3), cex=cex.category)
+  
+  
+  polygon(SAI_sd[,1], SAI_sd[,2], col =  adjustcolor("purple",alpha.f = 0.5), border = "white")
+  text(mean(SAI_sd[,1]),mean(SAI_sd[,2]), "SAI", cex = cex.text)
+  
+  polygon(LAI_sd[,1], LAI_sd[,2], col =  adjustcolor("purple",alpha.f = 0.5), border = "white")
+  text(mean(LAI_sd[,1]),mean(LAI_sd[,2]), "LAI", cex = cex.text)
+  
+  polygon(biom[,1], biom[,2], col =  adjustcolor("purple",alpha.f = 0.5), border = "white")
+  text(mean(biom[,1]),mean(biom[,2]), "ORTHOPTERA", cex = cex.text)
+  
+  # ---- 3. VEGETATION  ---- 
+  Diver=cbind(c(9,25,25,9), c(28,28,32,32))
+  Heter=cbind(c(9,25,25,9), c(34,34,38,38))
+  Height=cbind(c(9,25,25,9), c(40,40,44,44))
+  Cover=cbind(c(9,25,25,9), c(46,46,50,50))
+  Cover_dead=cbind(c(9,25,25,9), c(52,52,56,56))
+  
+  veg= rbind(Diver,Heter,Height,Cover,Cover_dead)
+  min1 <- min(veg[,1])-offset.poly
+  min2 <- min(veg[,2])-offset.poly
+  max1 <- max(veg[,1])+offset.poly
+  max2 <- max(veg[,2])+4
+  
+  polygon(c(min1,max1,max1,min1) ,
+          c(min2,min2,max2,max2)  ,
+          col =  adjustcolor("green",alpha.f = 0.1), border = "white")
+  text(mean(veg[,1]),max(veg[,2])+2 , "VEGETATION", col=grey(0.3), cex=cex.category)
+  
+  
+  polygon(Diver[,1], Diver[,2], col =  adjustcolor("green",alpha.f = 0.5), border = "white")
+  text(mean(Diver[,1]),mean(Diver[,2]), "DIVERSITY", cex = cex.text)
+  
+  polygon(Heter[,1], Heter[,2], col =  adjustcolor("green",alpha.f = 0.5), border = "white")
+  text(mean(Heter[,1]),mean(Heter[,2]), "HETEROGENEITY", cex = cex.text)
+  
+  polygon(Height[,1], Height[,2], col =  adjustcolor("green",alpha.f = 0.5), border = "white")
+  text(mean(Height[,1]),mean(Height[,2]), "HEIGHT", cex = cex.text)
+  
+  polygon(Cover[,1], Cover[,2], col =  adjustcolor("green",alpha.f = 0.5), border = "white")
+  text(mean(Cover[,1]),mean(Cover[,2]), "COVER", cex = cex.text)
+  
+  polygon(Cover_dead[,1], Cover_dead[,2], col =  adjustcolor("green",alpha.f = 0.5), border = "white")
+  text(mean(Cover_dead[,1]),mean(Cover_dead[,2]), "COVER_DEAD", cex = cex.text)
+  
+  # ---- 4. VEGETATION  2 ---- 
+  Diver1=cbind(c(-22,-6,-6,-22), c(-22,-22,-18,-18))
+  Heter1=cbind(c(-22,-6,-6,-22), c(-16,-16,-12,-12))
+  Height1=cbind(c(-22,-6,-6,-22), c(-10,-10,-6,-6))
+  Cover1=cbind(c(-22,-6,-6,-22), c(-4,-4,0,0))
+  Cover_dead1=cbind(c(-22,-6,-6,-22), c(2,2,6,6))
+  
+  veg1= rbind(Diver1,Heter1,Height1,Cover1,Cover_dead1)
+  min1 <- min(veg1[,1])-offset.poly
+  min2 <- min(veg1[,2])-offset.poly
+  max1 <- max(veg1[,1])+offset.poly
+  max2 <- max(veg1[,2])+4
+  
+  
+  polygon(c(min1,max1,max1,min1) ,
+          c(min2,min2,max2,max2)  ,
+          col =  adjustcolor("green",alpha.f = 0.1), border = "white")
+  text(mean(veg1[,1]),max(veg1[,2])+2 , "VEGETATION", col=grey(0.3), cex=cex.category)
+  
+  
+  polygon(Diver1[,1], Diver1[,2], col =  adjustcolor("green",alpha.f = 0.5), border = "white")
+  text(mean(Diver1[,1]),mean(Diver1[,2]), "DIVERSITY", cex = cex.text)
+  
+  polygon(Heter1[,1], Heter1[,2], col =  adjustcolor("green",alpha.f = 0.5), border = "white")
+  text(mean(Heter1[,1]),mean(Heter1[,2]), "HETEROGENEITY", cex = cex.text)
+  
+  polygon(Height1[,1], Height1[,2], col =  adjustcolor("green",alpha.f = 0.5), border = "white")
+  text(mean(Height1[,1]),mean(Height1[,2]), "HEIGHT", cex = cex.text)
+  
+  polygon(Cover1[,1], Cover1[,2], col =  adjustcolor("green",alpha.f = 0.5), border = "white")
+  text(mean(Cover1[,1]),mean(Cover1[,2]), "COVER", cex = cex.text)
+  
+  polygon(Cover_dead1[,1], Cover_dead1[,2], col =  adjustcolor("green",alpha.f = 0.5), border = "white")
+  text(mean(Cover_dead1[,1]),mean(Cover_dead1[,2]), "COVER_DEAD", cex = cex.text)
+  
+  
+  # ---- 5. LANDSCAPE  ---- 
+  tbl=cbind(c(40,54,54,40), c(-4,-4,0,0))
+  par=cbind(c(40,54,54,40), c(-10,-10,-6,-6))
+  Fallow=cbind(c(40,54,54,40), c(-16,-16,-12,-12))
+  Irrig=cbind(c(40,54,54,40), c(-22,-22,-18,-18))
+  area=cbind(c(51,69,69,51), c(52,52,56,56))
+  
+  
+  landscape= rbind(tbl,par,Fallow,Irrig)
+  min1 <- min(landscape[,1])-offset.poly
+  min2 <- min(landscape[,2])-offset.poly
+  max1 <- max(landscape[,1])+offset.poly
+  max2 <- max(landscape[,2])+4
+  
+  
+  polygon(c(min1,max1,max1,min1) ,
+          c(min2,min2,max2,max2)  ,
+          col =  adjustcolor("brown",alpha.f = 0.1), border = "white")
+  
+  text(mean(landscape[,1]),max(landscape[,2])+2 , "LANDSCAPE", col=grey(0.3), cex=cex.category)
+  
+  polygon(tbl[,1], tbl[,2], col =  adjustcolor("brown",alpha.f = 0.5), border = "white")
+  text(mean(tbl[,1]),mean(tbl[,2]), "TBL", cex = cex.text)
+  
+  
+  polygon(par[,1], par[,2], col =  adjustcolor("brown",alpha.f = 0.5), border = "white")
+  text(mean(par[,1]),mean(par[,2]), "PAR", cex = cex.text)
+  
+  polygon(Fallow[,1], Fallow[,2], col =  adjustcolor("brown",alpha.f = 0.5), border = "white")
+  text(mean(Fallow[,1]),mean(Fallow[,2]), "FALLOW", cex = cex.text)
+  
+  polygon(Irrig[,1], Irrig[,2], col =  adjustcolor("brown",alpha.f = 0.5), border = "white")
+  text(mean(Irrig[,1]),mean(Irrig[,2]), "IRRIGATION", cex = cex.text)
+  
+  polygon(area[,1], area[,2], col =  adjustcolor("yellow",alpha.f = 0.5), border = "white")
+  text(mean(area[,1]),mean(area[,2]), "FIELD AREA", cex = cex.text)
+  
+  
+  # ==== II. ADD THE ARROW ====  Define position of beginning and end of arrows (NO LANDSCAPE)
+  #FROM Treatment 
+  
+  predictors <- c( rep("Treatment", 8),
+                   "Diver", "Heter", "Height", "Cover", "Cover_dead",
+                   "Diver", "Heter", "Height", "Cover", "Cover_dead",
+                   "tbl","par","Fallow","Irrig",
+                   "SAI_sd", "LAI_sd", "biom",
+                   "LAI_sd", 
+                   "area")
+  
+  response <- c("SAI_sd","LAI_sd","biom","Diver","Heter","Height","Cover","Cover_dead",
+                rep("Pres",5),
+                rep("biom", 5),
+                rep("biom", 4),
+                rep("Pres",3),
+                "biom",
+                "Pres")
+  
+  x0= c(rep(mean(Treatment[c(1:2),1]),8),
+        mean(Diver[c(2),1]), mean(Heter[c(2),1]), mean(Height[c(2),1]), mean(Cover[c(2),1]), mean(Cover_dead[c(2),1]),
+        mean(Diver1[c(2),1]), mean(Heter1[c(2),1]), mean(Height1[c(2),1]), mean(Cover1[c(2),1]), mean(Cover_dead1[c(2),1]),
+        mean(tbl[c(1),1]), mean(par[c(1),1]), mean(Fallow[c(1),1]), mean(Irrig[c(1),1]),
+        SAI_sd[3,1], LAI_sd[3,1], biom[3,1],
+        mean(LAI_sd[1:2,1]),
+        mean(area[1:2,1]))
+  
+  x1= c(SAI_sd[1,1] , LAI_sd[1,1], biom[1,1], Diver[1,1], Heter[1,1], Height[1,1], Cover[1,1], Cover_dead[1,1],
+        rep(mean(Pres[1:2,1]), 5),
+        rep(mean(biom[1,1]), 5),
+        rep(mean(biom[2,1]), 4),
+        rep(Pres[1,1],3),
+        mean(biom[1:2,1]),
+        mean(area[1:2,1]) )
+  
+  y0= c(mean(Treatment[1,2]),mean(Treatment[1,2]),mean(Treatment[1,2]),
+        mean(Treatment[3,2]),mean(Treatment[3,2]),mean(Treatment[3,2]),mean(Treatment[3,2]),mean(Treatment[3,2]),
+        mean(Diver[3:2,2]), mean(Heter[3:2,2]), mean(Height[3:2,2]), mean(Cover[3:2,2]), mean(Cover_dead[3:2,2]),
+        mean(Diver1[3:2,2]), mean(Heter1[3:2,2]), mean(Height1[3:2,2]), mean(Cover1[3:2,2]), mean(Cover_dead1[3:2,2]),
+        mean(tbl[3:2,2]), mean(par[3:2,2]), mean(Fallow[3:2,2]), mean(Irrig[3:2,2]),
+        SAI_sd[3,2], LAI_sd[3,2], biom[3,2],
+        LAI_sd[2,2],
+        mean(area[2,2]) )
+  
+  
+  y1= c(mean(SAI_sd[c(1,4),2]) ,mean(LAI_sd[c(1,4),2]) ,mean(biom[c(1,4),2]) ,mean(Diver[c(1,4),2]),
+        mean(Heter[c(1,4),2]), mean(Height[c(1,4),2]), mean(Cover[c(1,4),2]), mean(Cover_dead[c(1,4),2]),
+        rep(mean(Pres[c(4),2]), 5),
+        rep(mean(biom[c(1:4),2]), 5),
+        rep(mean(biom[c(2:3),2]), 4),
+        rep(Pres[1,2],3),
+        biom[3,2],
+        mean(Pres[3,2]) )
+  
+  let <- c("D", "D", "D", "C", "C", "B", "B", "A", "L", "L", "K", "K", "J", 
+           "G", "G", "G", "G", "G", "I", "I", "I", "I", "M", "M", "M", "H", "O")
+  
+#  for(i in 1:length(x0)){ # For all grey
+    
+#      arrows(x0=x0[i], x1=x1[i], y0=y0[i], y1= y1[i],length = 0.1, lwd=1, col= "grey" )
+#      draw.circle(mean(c(x0[i], x1[i])), mean(c(y0[i], y1[i])), radius = 1.3, border = "grey",
+#                  col = "white")
+#      text(mean(c(x0[i], x1[i])) , mean(c(y0[i], y1[i])),let[i], cex = 0.7, col = "grey")
+     
+#  }
+  
+  #By colours ( = PlotMeta)
+  
+  for(i in 1:8){ 
+    
+    arrows(x0=x0[i], x1=x1[i], y0=y0[i], y1= y1[i],length = 0.1, lwd=1, col= "grey" )
+    draw.circle(mean(c(x0[i], x1[i])), mean(c(y0[i], y1[i])), radius = 1.3, border = "darkorange", col = "white")
+    text(mean(c(x0[i], x1[i])) , mean(c(y0[i], y1[i])),let[i], cex = 0.7, col = "darkorange")
+    
+  }
+  
+  for(i in 9:13){ 
+    
+    arrows(x0=x0[i], x1=x1[i], y0=y0[i], y1= y1[i],length = 0.1, lwd=1, col= "grey" )
+    draw.circle(mean(c(x0[i], x1[i])), mean(c(y0[i], y1[i])), radius = 1.3, border = adjustcolor("blue",alpha.f = 0.5), 
+                col = "white")
+    text(mean(c(x0[i], x1[i])) , mean(c(y0[i], y1[i])),let[i], cex = 0.7, col = adjustcolor("blue",alpha.f = 0.5))
+    
+  }
+  
+  for(i in 14:22){ 
+    
+    arrows(x0=x0[i], x1=x1[i], y0=y0[i], y1= y1[i],length = 0.1, lwd=1, col= "grey" )
+    draw.circle(mean(c(x0[i], x1[i])), mean(c(y0[i], y1[i])), radius = 1.3, border = adjustcolor("purple",alpha.f = 0.5), 
+                col = "white")
+    text(mean(c(x0[i], x1[i])) , mean(c(y0[i], y1[i])),let[i], cex = 0.7, col = adjustcolor("purple",alpha.f = 0.5))
+    
+  }
+  
+  for(i in 23:25){ 
+    
+    arrows(x0=x0[i], x1=x1[i], y0=y0[i], y1= y1[i],length = 0.1, lwd=1, col= "grey" )
+    draw.circle(mean(c(x0[i], x1[i])), mean(c(y0[i], y1[i])), radius = 1.3, border = adjustcolor("blue",alpha.f = 0.5), 
+                col = "white")
+    text(mean(c(x0[i], x1[i])) , mean(c(y0[i], y1[i])),let[i], cex = 0.7, col = adjustcolor("blue",alpha.f = 0.5))
+    
+  }
+  
+  i = 26
+  arrows(x0=x0[i], x1=x1[i], y0=y0[i], y1= y1[i],length = 0.1, lwd=1, col= "grey" )
+  draw.circle(mean(c(x0[i], x1[i])), mean(c(y0[i], y1[i])) + 0.5, radius = 1.3, border = adjustcolor("purple",alpha.f = 0.5), 
+              col = "white")
+  text(mean(c(x0[i], x1[i])) , mean(c(y0[i], y1[i])) + 0.5,let[i], cex = 0.7, col = adjustcolor("purple",alpha.f = 0.5))
+  
+  i = 27
+  arrows(x0=x0[i], x1=x1[i], y0=y0[i], y1= y1[i],length = 0.1, lwd=1, col= "grey" )
+  draw.circle(mean(c(x0[i], x1[i])), mean(c(y0[i], y1[i])), radius = 1.3, border = adjustcolor("blue",alpha.f = 0.5), 
+              col = "white")
+  text(mean(c(x0[i], x1[i])) , mean(c(y0[i], y1[i])),let[i], cex = 0.7, col = adjustcolor("blue",alpha.f = 0.5))
+  
+  #NOW, ADD LANDSCAPE ARROWS. AQUI
+  
+   
+    arrows(x0=mean(Pres[1:2,1])+6, x1=mean(Pres[1:2,1])+6, y0=mean(Irrig[3:2,2]), y1= mean(Pres[c(2),2]), length = 0.1, 
+           lwd=1, col= "grey")
+    segments(x0=mean(Irrig[2,1]), x1=mean(Pres[1:2,1])+6, y0=mean(Irrig[3:2,2]), y1= mean(Irrig[3:2,2]),
+             lwd=1, col= "grey")
+    draw.circle(mean(c(mean(Irrig[2,1]), mean(Pres[1:2,1])+6))-2.5, mean(c(mean(Irrig[3:2,2]), mean(Irrig[3:2,2]))), radius = 1.3, 
+                     border = adjustcolor("blue",alpha.f = 0.5), col = "white")
+    text(mean(c(mean(Irrig[2,1]), mean(Pres[1:2,1])+6))-2.5 , mean(c(mean(mean(Irrig[3:2,2])), mean(Irrig[3:2,2]))),"N", cex = 0.7, 
+         col = adjustcolor("blue",alpha.f = 0.5))
+    
+
+  
+    arrows(x0=mean(Pres[1:2,1])+4, x1=mean(Pres[1:2,1])+4, y0=mean(Fallow[3:2,2]), y1= mean(Pres[c(2),2]), length = 0.1, 
+           lwd=1, col= "grey")
+    segments(x0=mean(Fallow[2,1]), x1=mean(Pres[1:2,1])+4, y0=mean(Fallow[3:2,2]), y1= mean(Fallow[3:2,2]),
+             lwd=1, col= "grey")
+    draw.circle(mean(c(mean(Irrig[2,1]), mean(Pres[1:2,1])+6))-2.5, mean(c(mean(Fallow[3:2,2]), mean(Fallow[3:2,2]))), radius = 1.3, 
+                border = adjustcolor("blue",alpha.f = 0.5), col = "white")
+    text(mean(c(mean(Irrig[2,1]), mean(Pres[1:2,1])+6))-2.5 , mean(c(mean(Fallow[3:2,2]), mean(Fallow[3:2,2]))),"N", cex = 0.7, 
+         col = adjustcolor("blue",alpha.f = 0.5))
+    
+    
+    
+    arrows(x0=mean(Pres[1:2,1])+1, x1=mean(Pres[1:2,1])+1, y0=mean(par[3:2,2]), y1= mean(Pres[c(2),2]), length = 0.1, 
+           lwd=1, col="grey")
+    segments(x0=mean(par[2,1]), x1=mean(Pres[1:2,1])+1, y0=mean(par[3:2,2]), y1= mean(par[3:2,2]),
+             lwd=1, col="grey")
+    draw.circle(mean(c(mean(Irrig[2,1]), mean(Pres[1:2,1])+6))-2.5, mean(c(mean(par[3:2,2]), mean(par[3:2,2]))), radius = 1.3, 
+                border = adjustcolor("blue",alpha.f = 0.5), col = "white")
+    text(mean(c(mean(Irrig[2,1]), mean(Pres[1:2,1])+6))-2.5 , mean(c(mean(par[3:2,2]), mean(par[3:2,2]))),"N", cex = 0.7, 
+         col = adjustcolor("blue",alpha.f = 0.5))
+    
+    
+    
+    arrows(x0=mean(Pres[1:2,1])-1, x1=mean(Pres[1:2,1])-1, y0=mean(tbl[3:2,2]), y1= mean(Pres[c(2),2]), length = 0.1, 
+           lwd=1, col= "grey")
+    segments(x0=mean(tbl[2,1]), x1=mean(Pres[1:2,1])-1, y0=mean(tbl[3:2,2]), y1= mean(tbl[3:2,2]),
+             lwd=1, col= "grey")
+    draw.circle(mean(c(mean(Irrig[2,1]), mean(Pres[1:2,1])+6))-2.5, mean(c(mean(tbl[3:2,2]), mean(tbl[3:2,2]))), radius = 1.3, 
+                border = adjustcolor("blue",alpha.f = 0.5), col = "white")
+    text(mean(c(mean(Irrig[2,1]), mean(Pres[1:2,1])+6))-2.5 , mean(c(mean(tbl[3:2,2]), mean(tbl[3:2,2]))),"N", cex = 0.7, 
+         col = adjustcolor("blue",alpha.f = 0.5))
+    
+    
+
+    dev.off()
+  
+  
+  
+  
+  
+
+
+
